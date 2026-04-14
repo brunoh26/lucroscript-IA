eda40caf4dc01b3d43bd40234897bb586ca75578
brunoh26 <bh7919182@gmail.com>
2026-04-14 14:19:53 -0300

    Fix import statement and formatting in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Dashboard from './Dashboard' // Aqui chamamos a plataforma (Painel)
+import Dashboard from './Dashboard'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Dashboard />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
